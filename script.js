1cab8f8a0d83d46e26e8d52b86994e84be623551
Ruban32 <rubanaids@gmail.com>
2026-05-29 18:53:13 +0530

    Update script.js
    updated

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-function addTask(){
+function addTask() {
 
   const input = document.getElementById("taskInput");
 
   const taskText = input.value.trim();
 
-  if(taskText === ""){
+  if (taskText === "") {
     return;
   }
 
   const li = document.createElement("li");
 
-  li.innerHTML = `
-    <span onclick="toggleTask(this)">
-      ${taskText}
-    </span>
+  const span = document.createElement("span");
 
-    <button onclick="deleteTask(this)">
-      Delete
-    </button>
-  `;
+  span.innerText = taskText;
+
+  span.onclick = function () {
+    span.style.textDecoration = "line-through";
+  };
+
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.innerText = "Delete";
+
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(span);
+
+  li.appendChild(deleteBtn);
 
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";
-}
-
-function deleteTask(button){
-  button.parentElement.remove();
-}
-
-function toggleTask(task){
-  task.classList.toggle("completed");
 }
