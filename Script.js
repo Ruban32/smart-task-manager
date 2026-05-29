d9462393abed3c16c9cf99e13e31ba741b9dbe2d
Ruban32 <rubanaids@gmail.com>
2026-05-29 18:21:33 +0530

    new changes
    changes

---
+++ Script.js
@@ -0,0 +1,43 @@
+function addTask(){
+
+  const input = document.getElementById("taskInput");
+
+  const taskText = input.value.trim();
+
+  if(taskText === ""){
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+  
+    <span 
+    class="task-text"
+    onclick="toggleTask(this)">
+      ${taskText}
+    </span>
+
+    <button 
+    class="delete-btn"
+    onclick="deleteTask(this)">
+      Delete
+    </button>
+
+  `;
+
+  document
+  .getElementById("taskList")
+  .appendChild(li);
+
+  input.value = "";
+}
+
+function deleteTask(button){
+  button.parentElement.remove();
+}
+
+function toggleTask(task){
+  task.classList.toggle("completed");
+}
